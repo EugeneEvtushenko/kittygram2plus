c4e63558a55a2ff91567b3841d149eedd2f077ec
Eugene Evtushenko <eugene.evtushenko@gmail.com>
2026-01-14 00:08:23 +0500

    lesson 2 done

--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,35 @@
 from rest_framework import viewsets
+from rest_framework.throttling import AnonRateThrottle
+from rest_framework.throttling import ScopedRateThrottle
 
-from .models import Achievement, Cat, User
 
-from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from cats.models import Achievement, Cat, User
+from cats.permissions import OwnerOrReadOnly, ReadOnly
+from cats.serializers import AchievementSerializer, CatSerializer, UserSerializer
+from cats.throttling import WorkingHoursRateThrottle
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    # Устанавливаем разрешение
+    permission_classes = (OwnerOrReadOnly,)
+    # throttle_classes = (AnonRateThrottle,)  # Подключили класс AnonRateThrottle
+    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+
+    # Для любых пользователей установим кастомный лимит 1 запрос в минуту
+    throttle_scope = 'low_request'
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+        # Если в GET-запросе требуется получить информацию об объекте
+        if self.action == 'retrieve':
+            # Вернём обновлённый перечень используемых пермишенов
+            return (ReadOnly(),)
+        # Для остальных ситуаций оставим текущий перечень пермишенов без изменений
+        return super().get_permissions()
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
