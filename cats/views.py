18c56396d2ec0ec23908e952738ea37f026c9300
Eugene Evtushenko <eugene.evtushenko@gmail.com>
2026-01-14 18:24:49 +0500

    done

--- cats/views.py
+++ cats/views.py
@@ -1,11 +1,14 @@
-from rest_framework import viewsets
-from rest_framework.throttling import AnonRateThrottle
-from rest_framework.throttling import ScopedRateThrottle
-
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
+from rest_framework.pagination import (LimitOffsetPagination,
+                                       PageNumberPagination)
+from rest_framework.throttling import AnonRateThrottle, ScopedRateThrottle
 
 from cats.models import Achievement, Cat, User
+from cats.pagination import CatsPagination
 from cats.permissions import OwnerOrReadOnly, ReadOnly
-from cats.serializers import AchievementSerializer, CatSerializer, UserSerializer
+from cats.serializers import (AchievementSerializer, CatSerializer,
+                              UserSerializer)
 from cats.throttling import WorkingHoursRateThrottle
 
 
@@ -16,9 +19,20 @@ class CatViewSet(viewsets.ModelViewSet):
     permission_classes = (OwnerOrReadOnly,)
     # throttle_classes = (AnonRateThrottle,)  # Подключили класс AnonRateThrottle
     throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    # Указываем фильтрующий бэкенд DjangoFilterBackend
+    # Из библиотеки django-filter
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    # Фильтровать будем по полям color и birth_year модели Cat
+    filterset_fields = ('color', 'birth_year')
+    # Доступные для поиска поля, в т.ч. поля связанной модели достижений и хозяина
+    search_fields = ('name', 'achievements__name', 'owner__username')
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',)
 
     # Для любых пользователей установим кастомный лимит 1 запрос в минуту
     throttle_scope = 'low_request'
+    # Вот он наш собственный класс пагинации с page_size=20
+    pagination_class = None
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
